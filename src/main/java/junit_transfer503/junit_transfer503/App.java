840ce5fbbb7aab3f2d506fa017fc75a42e14a51f
91830 <91830@srivalli>
2022-11-24 20:47:48 +0530

    junit-jenkins

---
+++ src/main/java/junit_transfer503/junit_transfer503/App.java
@@ -0,0 +1,17 @@
+package junit_transfer503.junit_transfer503;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
